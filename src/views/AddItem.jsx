46a973e022522adc7b9e8e336f6ddf477353f574
Cassandra Umeh <umehcassandra@gmail.com>
2023-04-12 15:38:08 +0100

    build form for adding new items to the shopping list

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,81 @@
+import React, { useState } from 'react';
+
 export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+	const [formData, setFormData] = useState({
+		itemData: '',
+		dateNextPurchased: ' ',
+	});
+	function handleChange(event) {
+		const { name, value, checked, type } = event.target;
+		setFormData((prevFormData) => {
+			return {
+				...prevFormData,
+				[name]: value,
+			};
+		});
+	}
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
+
+	return (
+		<form method="post" onSubmit={handleSubmit}>
+			<label>
+				Item name:
+				<br />
+				<input
+					type="text"
+					placeholder=""
+					name="itemData"
+					value={formData.itemName}
+					onChange={handleChange}
+				/>
+			</label>
+			<br />
+
+			<p> How soon will you buy this again? </p>
+
+			<label>
+				<input
+					type="radio"
+					id="soon"
+					name="dateNextPurchased"
+					value="7"
+					checked={formData.dateNextPurchased === '7'}
+					onChange={handleChange}
+				/>
+				Soon
+			</label>
+			<br />
+
+			<label>
+				<input
+					type="radio"
+					id="kind-of-soon"
+					name="dateNextPurchased"
+					value={14}
+					checked={formData.dateNextPurchased === '14'}
+					onChange={handleChange}
+				/>
+				kind of Soon
+			</label>
+			<br />
+
+			<label>
+				<input
+					type="radio"
+					id="not-soon"
+					name="dateNextPurchased"
+					value={30}
+					checked={formData.dateNextPurchased === '30'}
+					onChange={handleChange}
+				/>
+				Not Soon
+			</label>
+			<br />
+			<div>
+				<button>Add Item</button>
+			</div>
+		</form>
+	);
 }
