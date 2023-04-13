7d435c807510145cd94126c2324ad7a241d440c3
KarenJParker77 <karen.mccrackle@gmail.com>
2023-04-13 21:33:00 +0100

    added redirect to list view

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
 
 import { AddItem, Home, Layout, List } from './views';
 
@@ -59,8 +64,19 @@ export function App() {
 		<Router>
 			<Routes>
 				<Route path="/" element={<Layout />}>
-					<Route index element={<Home handleCreateList={handleCreateList} />} />
+					<Route
+						index
+						element={
+							listToken ? (
+								<Navigate to="/list" />
+							) : (
+								<Home handleCreateList={handleCreateList} />
+							)
+						}
+					/>
+
 					<Route path="/list" element={<List data={data} />} />
+
 					<Route path="/add-item" element={<AddItem />} />
 				</Route>
 			</Routes>
