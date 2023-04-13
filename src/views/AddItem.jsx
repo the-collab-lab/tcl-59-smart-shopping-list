d4713451fa10683189a9d37a46a5a0243a5b3663
mikeyxx <michaelogboo@gmail.com>
2023-04-13 09:09:36 +0100

    added the addItem function to the firebase and imported the same function the AddItem page

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,77 +1,83 @@
+import './AddItem.css';
 import React, { useState } from 'react';
+import { addItem } from '../api';
 
-export function AddItem() {
-	const [formData, setFormData] = useState({
-		itemData: '',
-		dateNextPurchased: ' ',
+export function AddItem({ listToken }) {
+	const [itemData, setItemData] = useState({
+		itemName: '',
+		daysUntilNextPurchase: '7',
 	});
 	function handleChange(event) {
-		const { name, value, checked, type } = event.target;
-		setFormData((prevFormData) => {
+		const { name, value } = event.target;
+		setItemData((prevFormData) => {
 			return {
 				...prevFormData,
 				[name]: value,
 			};
 		});
 	}
+
+	const { itemName, daysUntilNextPurchase } = itemData;
+
 	function handleSubmit(e) {
 		e.preventDefault();
+		addItem(listToken, itemName, parseInt(daysUntilNextPurchase));
+		console.log(itemName, parseInt(daysUntilNextPurchase));
 	}
 
 	return (
 		<form method="post" onSubmit={handleSubmit}>
-			<label>
-				Item name:
+			<label htmlFor="itemName">Item name:</label>
+			<br />
+			<input
+				type="text"
+				id="itemName"
+				name="itemName"
+				value={itemData.itemName}
+				onChange={handleChange}
+			/>
+
+			<fieldset>
+				<legend>How soon will you buy this again</legend>
+				<label htmlFor="soon">
+					<input
+						type="radio"
+						id="soon"
+						name="daysUntilNextPurchase"
+						value={7}
+						checked={itemData.daysUntilNextPurchase === '7'}
+						onChange={handleChange}
+					/>
+					Soon
+				</label>
 				<br />
-				<input
-					type="text"
-					placeholder=""
-					name="itemData"
-					value={formData.itemName}
-					onChange={handleChange}
-				/>
-			</label>
-			<br />
 
-			<p> How soon will you buy this again? </p>
+				<label htmlFor="kind-of-soon">
+					<input
+						type="radio"
+						id="kind-of-soon"
+						name="daysUntilNextPurchase"
+						value={14}
+						checked={itemData.daysUntilNextPurchase === '14'}
+						onChange={handleChange}
+					/>
+					kind of Soon
+				</label>
+				<br />
 
-			<label>
-				<input
-					type="radio"
-					id="soon"
-					name="dateNextPurchased"
-					value="7"
-					checked={formData.dateNextPurchased === '7'}
-					onChange={handleChange}
-				/>
-				Soon
-			</label>
-			<br />
+				<label htmlFor="not-soon">
+					<input
+						type="radio"
+						id="not-soon"
+						name="daysUntilNextPurchase"
+						value={30}
+						checked={itemData.daysUntilNextPurchase === '30'}
+						onChange={handleChange}
+					/>
+					Not Soon
+				</label>
+			</fieldset>
 
-			<label>
-				<input
-					type="radio"
-					id="kind-of-soon"
-					name="dateNextPurchased"
-					value={14}
-					checked={formData.dateNextPurchased === '14'}
-					onChange={handleChange}
-				/>
-				kind of Soon
-			</label>
-			<br />
-
-			<label>
-				<input
-					type="radio"
-					id="not-soon"
-					name="dateNextPurchased"
-					value={30}
-					checked={formData.dateNextPurchased === '30'}
-					onChange={handleChange}
-				/>
-				Not Soon
-			</label>
 			<br />
 			<div>
 				<button>Add Item</button>
