aed37324dfb896838eaaccbcb48eb2a7ffe47802
Cassandra Umeh <umehcassandra@gmail.com>
2023-04-26 18:39:38 +0100

    Add a spinner when loading list page

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ListItem } from '../components';
 import { Link } from 'react-router-dom';
+import BeatLoader from 'react-spinners/BeatLoader';
 
-export function List({ data }) {
+export function List({ data, isLoading }) {
+	console.log(data);
 	const [searchQuery, setSearchQuery] = useState('');
 
 	const filteredData = data.filter((item) =>
 		item.name.toLowerCase().includes(searchQuery.toLowerCase()),
 	);
-
+	if (isLoading) {
+		return <BeatLoader color="black" loading={isLoading} size={15} />;
+	}
 	return (
 		<>
 			{data.length === 0 ? (
@@ -29,7 +33,6 @@ export function List({ data }) {
 						placeholder="Start typing here"
 						onChange={(e) => setSearchQuery(e.target.value)}
 					/>
-
 					<ul>
 						{filteredData.map((item) => (
 							<ListItem key={item.id} name={item.name} />
