25af98280e31c5b7d41f221d25f44d9f3657cbb5
Cassandra Umeh <umehcassandra@gmail.com>
2023-05-02 19:40:16 +0100

    Refactore setIsLoading

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import { generateToken } from '@the-collab-lab/shopping-list-utils';
 export function App() {
 	const [data, setData] = useState([]);
 	const [errorMsg, setErrorMsg] = useState('');
-	const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 	/**
 	 * Here, we're using a custom hook to create `listToken` and a function
 	 * that can be used to update `listToken` later.
@@ -33,7 +33,6 @@ export function App() {
 	);
 
 	useEffect(() => {
-		setIsLoading(true);
 		if (!listToken) return;
 
 		/**
